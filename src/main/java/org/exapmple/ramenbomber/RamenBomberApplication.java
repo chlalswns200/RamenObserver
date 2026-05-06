dabbe134c4b39fbafe8904859ba3fe7033c077bf
choiminjun <chlalswns200@naver.com>
2026-05-06 22:47:42 +0900

    chore: 프로젝트 초기 설정 추가

---
+++ src/main/java/org/exapmple/ramenbomber/RamenBomberApplication.java
@@ -0,0 +1,13 @@
+package org.exapmple.ramenbomber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenBomberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamenBomberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exapmple/ramenbomber/RamenBomberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exapmple.ramenbomber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamenBomberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
